4eb82a45b7105cfd3568e368ff5414d00c73c817
Angel <angel.sole.h@gmail.com>
2020-03-24 12:43:12 +0100

    Remove unused EasyConfig dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EasyconfigModule } from  'nestjs-easyconfig';
 import { DatabaseModule } from './config/database.module';
 
 
 @Module({
   imports: [
-    EasyconfigModule.register({}),
     DatabaseModule,
   ],
   controllers: [AppController],
